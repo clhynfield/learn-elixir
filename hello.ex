e014db39bb9ae1ea2d45db8b71c7bdb8b8109776
Clayton Hynfield <clayton@hynfield.org>
2024-02-23 13:34:35 -0500

    Refactor: make it easier to support i18n

--- hello.ex
+++ hello.ex
@@ -1,7 +1,16 @@
 defmodule Greetings do
+  @english_greeting "Hello, "
+  @english_world "world"
+  @english_punctuation "!"
+
   @spec hello :: String.t()
   def hello do
-    "Hello, world!"
+    @english_greeting <> @english_world <> @english_punctuation
+  end
+
+  @spec hello(String.t()) :: String.t()
+  def hello(name) do
+    @english_greeting <> name <> @english_punctuation
   end
 
   @spec say_hello :: :ok
