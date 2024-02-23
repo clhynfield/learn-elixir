cde30fda5ada6a990ed19c818745e886dfdfe423
Clayton Hynfield <clayton@hynfield.org>
2024-02-23 13:34:50 -0500

    Greet the world by default

--- hello_test.exs
+++ hello_test.exs
@@ -6,7 +6,10 @@ defmodule GreetingsTest do
   test "greets the world" do
     assert Greetings.hello() == "Hello, world!"
   end
-  test "greets people" do
+  test "greets people by name" do
     assert Greetings.hello("Clayton") == "Hello, Clayton!"
+  end
+  test "greets the world by default" do
+    assert Greetings.hello("") == "Hello, world!"
   end
 end
